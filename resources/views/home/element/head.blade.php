6e430fd7534aff4b9ad519413a7fe4b82633a80a
DungRoyal <Doanquodung55@gmail.com - 0398022720>
2020-06-09 23:35:06 +0700

    :fu:

--- resources/views/home/element/head.blade.php
+++ resources/views/home/element/head.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Tuyển dụng | Tìm Việc Làm nhanh,hiệu quả | Timvieclam.xyz</title>
-
+    <link rel="icon" href="{{ asset('home') }}/img/favicon.png" type="image/gif" sizes="16x16">
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap" rel="stylesheet">
 
