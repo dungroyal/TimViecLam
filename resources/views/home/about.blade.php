0e92893af6f302a9f584551a96ecf38a5edfcd82
Lê Hào <57648805+LeeHao1999@users.noreply.github.com>
2020-06-30 07:46:03 +0700

    .

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -24,18 +24,15 @@
             <div class="row">
                 <div class="col-lg-5 col-md-5">
                     <div class="about__title">
-                        <h2>Welcome to<br /> Directing Listing</h2>
+                        <h2>Chào mừng bạn đến<br /> Danh sách chỉ đạo</h2>
                         <a href="/contact" class="primary-btn">Liên hệ với chúng tôi</a>
                     </div>
                 </div>
                 <div class="col-lg-7 col-md-7">
                     <div class="about__text">
-                        <h5>Metasurfaces are generally designed by placing scatterers in periodic or pseudo-periodic
-                            grids.</h5>
-                        <p>You have finished building your own website. You have introduced your company and presented
-                            your products and services. You have added propositions and promos to catch your target
-                            audience’s attention. You think you are doing everything “right”, but all your promotions
-                            have failed to produce growth in your new internet business.</p>
+                        <h5>Metasurfaces thường được thiết kế bằng cách đặt các bộ phân tán trong các lưới định kỳ hoặc giả định kỳ.</h5>
+                        <p>
+Bạn đã hoàn thành việc xây dựng trang web của riêng bạn. Bạn đã giới thiệu công ty của bạn và trình bày các sản phẩm và dịch vụ của bạn. Bạn đã thêm các đề xuất và khuyến mãi để thu hút sự chú ý của đối tượng mục tiêu của bạn. Bạn nghĩ rằng bạn đang làm mọi thứ ngay lập tức, nhưng tất cả các chương trình khuyến mãi của bạn đã thất bại trong việc tạo ra sự tăng trưởng trong kinh doanh internet mới của bạn.</p>
                     </div>
                 </div>
             </div>
@@ -63,8 +60,8 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="section-title">
-                        <h2>How Does It Work</h2>
-                        <p>Travelocity empowers travelers who are giving back on their trips in ways big and small</p>
+                        <h2>Làm thế nào nó hoạt động</h2>
+                        <p>EDT trao quyền cho khách du lịch đang trả lại chuyến đi của họ theo cách lớn và nhỏ</p>
                     </div>
                 </div>
             </div>
@@ -73,27 +70,24 @@
                     <div class="work__item">
                         <div class="work__item__number">01.</div>
                         <img src="{{ asset('home') }}/img/work/work-1.png" alt="">
-                        <h5>Location & Categories</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore.</p>
+                        <h5>Địa điểm & danh mục</h5>
+                        <p>Các địa điểm và danh mục được sắp xếp với nhau theo trình tự nhất định đễ có thể hoạt động dễ dàng hơn.</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6">
                     <div class="work__item">
                         <div class="work__item__number">02.</div>
                         <img src="{{ asset('home') }}/img/work/work-2.png" alt="">
-                        <h5>Explore Listting</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore.</p>
+                        <h5>Khám phá</h5>
+                        <p>Cùng nhau khám phá những điều mới mẻ hơn trong cuộc sống và công việc.</p>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6">
                     <div class="work__item">
                         <div class="work__item__number">03.</div>
                         <img src="{{ asset('home') }}/img/work/work-3.png" alt="">
-                        <h5>Making Appointments</h5>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                            labore.</p>
+                        <h5>Đăng ký cuộc hẹn</h5>
+                        <p>Hãy liên hệ với chúng tôi ngay bạn nhé.</p>
                     </div>
                 </div>
             </div>
@@ -107,42 +101,30 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="section-title">
-                        <h2>Trusted By Over 5000+ User</h2>
-                        <p>What people say about us</p>
+                        <h2>
+Được tin cậy bởi hơn 5000 người dùng</h2>
+                        <p>
+Mọi người nói gì về chúng tôi</p>
                     </div>
                     <div class="testimonial__slider owl-carousel">
                         <div class="testimonial__item" data-hash="review-1">
-                            <p>" We worked with Consultant. Our representative was very knowledgeable and helpful.
-                                Consultant made a number of suggestions to help improve our systems. Consultant
-                                explained how things work and why it would help."</p>
-                            <div class="testimonial__item__author">
-                                <a href="#review-3"><img src="{{ asset('home') }}/img/testimonial/author-3.png" alt=""></a>
-                                <a href="#review-1" class="active"><img src="{{ asset('home') }}/img/testimonial/author-1.png" alt=""></a>
-                                <a href="#review-2"><img src="{{ asset('home') }}/img/testimonial/author-2.png" alt=""></a>
-                            </div>
+                            <p>" Chúng tôi đã làm việc với Tư vấn. Đại diện của chúng tôi rất hiểu biết và hữu ích.
+                                Tư vấn đã đưa ra một số gợi ý để giúp cải thiện hệ thống của chúng tôi. Tư vấn
+                                giải thích cách mọi thứ hoạt động và tại sao nó sẽ giúp."</p>
+                            
                             <div class="testimonial__item__author__text">
-                                <h5>John Smith -</h5>
-                                <div class="testimonial__item__author__rating">
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                    <i class="fa fa-star"></i>
-                                </div>
+                                <h5>Đoàn Quốc Dũng -</h5>
+                                
                             </div>
-                            <span>CEO Colorlib</span>
+                            <span>CEO Lê Nhật Hào</span>
                         </div>
                         <div class="testimonial__item" data-hash="review-2">
-                            <p>" We worked with Consultant. Our representative was very knowledgeable and helpful.
-                                Consultant made a number of suggestions to help improve our systems. Consultant
-                                explained how things work and why it would help."</p>
-                            <div class="testimonial__item__author">
-                                <a href="#review-1"><img src="{{ asset('home') }}/img/testimonial/author-1.png" alt=""></a>
-                                <a href="#review-2" class="active"><img src="{{ asset('home') }}/img/testimonial/author-2.png" alt=""></a>
-                                <a href="#review-3"><img src="{{ asset('home') }}/img/testimonial/author-3.png" alt=""></a>
-                            </div>
+                            <p>" Chúng tôi đã làm việc với Tư vấn. Đại diện của chúng tôi rất hiểu biết và hữu ích.
+                                Tư vấn đã đưa ra một số gợi ý để giúp cải thiện hệ thống của chúng tôi. Tư vấn
+                                giải thích cách mọi thứ hoạt động và tại sao nó sẽ giúp."</p>
+                            
                             <div class="testimonial__item__author__text">
-                                <h5>John Smith -</h5>
+                                <h5>Đoàn Quốc Dũng -</h5>
                                 <div class="testimonial__item__author__rating">
                                     <i class="fa fa-star"></i>
                                     <i class="fa fa-star"></i>
@@ -151,19 +133,15 @@
                                     <i class="fa fa-star"></i>
                                 </div>
                             </div>
-                            <span>CEO Colorlib</span>
+                            <span>CEO Lê Nhật Hào</span>
                         </div>
                         <div class="testimonial__item" data-hash="review-3">
-                            <p>" We worked with Consultant. Our representative was very knowledgeable and helpful.
-                                Consultant made a number of suggestions to help improve our systems. Consultant
-                                explained how things work and why it would help."</p>
-                            <div class="testimonial__item__author">
-                                <a href="#review-2"><img src="{{ asset('home') }}/img/testimonial/author-2.png" alt=""></a>
-                                <a href="#review-3" class="active"><img src="{{ asset('home') }}/img/testimonial/author-3.png" alt=""></a>
-                                <a href="#review-1"><img src="{{ asset('home') }}/img/testimonial/author-1.png" alt=""></a>
-                            </div>
+                            <p>" Chúng tôi đã làm việc với Tư vấn. Đại diện của chúng tôi rất hiểu biết và hữu ích.
+                                Tư vấn đã đưa ra một số gợi ý để giúp cải thiện hệ thống của chúng tôi. Tư vấn
+                                giải thích cách mọi thứ hoạt động và tại sao nó sẽ giúp."</p>
+                            
                             <div class="testimonial__item__author__text">
-                                <h5>John Smith -</h5>
+                                <h5>Đoàn Quốc Dũng -</h5>
                                 <div class="testimonial__item__author__rating">
                                     <i class="fa fa-star"></i>
                                     <i class="fa fa-star"></i>
@@ -172,7 +150,7 @@
                                     <i class="fa fa-star"></i>
                                 </div>
                             </div>
-                            <span>CEO Colorlib</span>
+                            <span>CEO Lê Nhật Hào</span>
                         </div>
                     </div>
                 </div>
